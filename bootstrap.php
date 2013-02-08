da4e0275d6c054149e47e5df61f6e1e1b2e95192
niaeashes <nia.eashes@gmail.com>
2013-02-08 14:32:08 +0900

    Create color and metal.

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 Autoloader::add_classes(array(
+    'Rookers\\Color' => __DIR__.'/classes/color.php',
     'Rookers\\Instance' => __DIR__.'/classes/instance.php',
     'Rookers\\Metal' => __DIR__.'/classes/metal.php'
 ));
---
+++ classes/instance.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Rookers;
+
+abstract class Instance
+{
+
+	protected static $data = array();
+
+	public static function forge($name)
+	{
+		if ( class_exists($class = '\\Rookers\\'.$name, true))
+		{
+			return new $class();
+		}
+
+		echo $class;
+
+		return false;
+	}
+
+	public function rand()
+	{
+		return ucfirst(static::$data[array_rand(static::$data)]);
+	}
+}
