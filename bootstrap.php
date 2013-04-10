7079fd67cad84bb95c0c38e3ec6c744ddf3ebfef
Tamaki <tmaeda@clustium.com>
2013-04-11 03:05:58 +0900

    Rebase.

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ Autoloader::add_classes(array(
     'Rookers\\Japanese' => __DIR__.'/classes/japanese.php',
     'Rookers\\Japanese_FirstName' => __DIR__.'/classes/japanese/firstname.php',
     'Rookers\\Japanese_LastName' => __DIR__.'/classes/japanese/lastname.php',
-    'Rookers\\Japanese\Pref' => __DIR__.'/classes/japanese/pref.php',
-    'Rookers\\Japanese\University' => __DIR__.'/classes/japanese/university.php',
+    'Rookers\\Japanese_Pref' => __DIR__.'/classes/japanese/pref.php',
+    'Rookers\\Japanese_University' => __DIR__.'/classes/japanese/university.php',
     'Rookers\\Metal' => __DIR__.'/classes/metal.php'
 ));
