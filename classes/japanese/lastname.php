a19815645ef89575a829354a5e13079cc3367ea2
niaeashes <nia.eashes@gmail.com>
2013-04-11 02:13:41 +0900

    Create japanese names.

---
+++ classes/japanese/lastname.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Rookers;
+
+class Japanese_LastName extends Instance
+{
+	protected static $data = array(
+		'安喰', '明星', '悪原', '我妻', '今留', '入口', '十六女', '右左', '雲龍院', '七草', '嘉月', '木枯', '九十九', '小鳥遊',
+		'雲母', '春夏秋冬', '六郎面', '本名', '棗田', '遊馬', '門家', '次村', '鬼崎', '神崎', '八色', '矢手又', '雪入', '百合草'
+	);
+}
